d63448035afa4d85260fcab1d3598ad0a5c163f7
Yoka0014 <yuichiro.okashita.5z@stu.hosei.ac.jp>
2023-05-31 15:46:30 +0900

    DQNの訓練において，対局をNNのバッチ数だけ並列に行うように変更. prob_reversi_helperにおけるメモリリークの不具合を修正． QNetworkにおいて，デストラクタ内でkeras.Modelオブジェクトを破棄するように修正．

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mc_player import MCPlayer
 from random_mover import RandomMover
 from dqn_player import DQNPlayer
 
-player_0, player_1 = RandomMover(), DQNPlayer("qnet_31.h5")
+player_0, player_1 = MCPlayer(), DQNPlayer("qnet_624.h5")
 # prob = [0.8, 0.2, 0.5, 0.5, 0.2, 0.8,
 #         0.2, 0.2, 0.5, 1.0, 0.2, 0.2,
 #         0.5, 0.5, 1.0, 1.0, 1.0, 0.5,
@@ -11,8 +11,26 @@ player_0, player_1 = RandomMover(), DQNPlayer("qnet_31.h5")
 #         0.8, 0.8, 1.0, 0.5, 0.8, 0.8,
 #         0.2, 0.8, 0.5, 0.5, 0.8, 0.2]
 # game = Game(player_0, player_1, 6, prob)
+
+def beep(freq, dur=100):
+    """
+        ビープ音を鳴らす.
+        @param freq 周波数
+        @param dur  継続時間（ms）
+    """
+    import platform
+    if platform.system() == "Windows":
+        import winsound
+        winsound.Beep(freq, dur)
+    else:
+        import os
+        os.system('play -n synth %s sin %s' % (dur/1000, freq))
+
+
 game = Game(player_0, player_1, 6, [1.0] * 36)
-game.start(200, swap_player_for_each_game=True, use_gui=False, gui_size=512)
+game.start(1000, swap_player_for_each_game=True, use_gui=False, gui_size=512)
+
+beep(2000, 1000)
 
 
 
